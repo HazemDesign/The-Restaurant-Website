d9cadae9218ad0cec6e1bc61c18816283db13b6f
Mustafa4Mohammed <mustafamhmd134@gmail.com>
2024-05-09 02:33:05 +0300

    Create menuData.js

---
+++ JavaScript/menuData.js
@@ -0,0 +1,62 @@
+var menuObjs =[
+    {name:"Baba Ghanoush",price:"",
+    img:"menu/appetizer/Baba Ghanoush.webp",
+    desc:"Roasted eggplant, garlic, tahini, lemon and extra virgin olive oil. Gluten free and vegan."},
+
+    {name:"Falafel",price:"",
+    img:"menu/appetizer/Falafel.webp",
+    desc:"Chickpeas, tahini, parsley and kuzbara. Gluten free and Vegan."},
+
+    {name:"Fattoush Salad",price:"",
+    img:"menu/appetizer/Fattoush Salad.webp",
+    desc:"Purslane, romaine lettuce, cucumber, tomatoes, radish, mint, pita chips, sumac, pomegranate, lemon and extra virgin olive oil. Vegan."}
+    ,
+    {name:"Hummus",price:"",
+    img:"menu/appetizer/Hummus.webp",
+    desc:"Chickpea puree, tahini, lemon and extra virgin olive oil. Gluten free and vegan"},
+
+
+    {name:"Muhammara",price:"",
+    img:"menu/appetizer/Muhammara.webp",
+    desc:"Walnuts, roasted peppers, pomegranate and extra virgin olive oil. Vegan."},
+
+    {name:"Arnabeet Mekle",price:"",
+    img:"menu/main/Arnabeet Mekle.webp",
+    desc:"Cauliflower, Aleppo pepper, lemon, garlic yogurt and tahini. Gluten free. Vegetarian."},
+
+    {name:"Batata Harra",price:"",
+    img:"menu/main/Batata Harra.webp",
+    desc:"Aleppo pepper, cilantro, herbs and garlic toum. Gluten free. Vegetarian."},
+
+    {name:"Beef Lule Kebab",price:"",
+    img:"menu/main/Beef Lule Kebab.webp",
+    desc:"Ground Angus beef, parsley and Aleppo pepper. Gluten free."},
+
+    {name:"Lamb Lule Kebab",price:"",
+    img:"menu/main/Lamb Lule Kebab.webp",
+    desc:"Ground lamb, red onion, thyme and sumac. Served Armenian pilaf, toasted vermicelli, almonds and grilled veggies."},
+
+    {name:"Mante",price:"",
+    img:"menu/main/Mante.webp",
+    desc:"Armenian beef dumpling, garlic yogurt sauce and sumac."},
+
+    {name:"Shawarma",price:"",
+    img:"menu/main/Shawarma.webp",
+    desc:"Tomato, onion, tahini, sumac, pickles. Gluten free."},
+
+    {name:"Shish Taouk",price:"",
+    img:"menu/main/Shish Taouk.webp",
+    desc:"Organic chicken breast and garlic toum. Gluten free."},
+
+    {name:"Vegetarian Flatbread",price:"",
+    img:"menu/main/Vegetarian Flatbread.webp",
+    desc:"Kashkaval cheese, Kalamata olives, cherry tomato, mushrooms, jalapenos and avocado. Vegetarian."},
+
+    {name:"Sujuk Flatbread",price:"",
+    img:"menu/main/Sujuk Flatbread.webp",
+    desc:"Spicy Armenian sausage, akawi cheese, halloumi cheese, 63-degree quail eggs, cherry tomatoes and basil"},
+
+    {name:"Za'atar Flatbread",price:"",
+    img:"menu/main/Za'atar Flatbread.webp",
+    desc:"Aleppo, za'atar, akawi cheese, cherry tomato, mint and extra virgin olive oil. Vegetarian."},
+];
