08a92672a1193054a2de8cf3647d85be591394ca
Mustafa4Mohammed <mustafamhmd134@gmail.com>
2024-05-09 14:44:20 +0300

    update

--- JavaScript/menuData.js
+++ JavaScript/menuData.js
@@ -1,62 +1,42 @@
 var menuObjs =[
-    {name:"Baba Ghanoush",price:"",
-    img:"menu/appetizer/Baba Ghanoush.webp",
+    {name:"Sayadieh fish-rice",price:"€120.00",
+    img:"/Images/Sayadieh-fish-and-rice1.jpg",
     desc:"Roasted eggplant, garlic, tahini, lemon and extra virgin olive oil. Gluten free and vegan."},
 
-    {name:"Falafel",price:"",
-    img:"menu/appetizer/Falafel.webp",
+    {name:"Grilled fish",price:"€110.00",
+    img:"'/Images/grilled-rainbow-trout.jpg",
     desc:"Chickpeas, tahini, parsley and kuzbara. Gluten free and Vegan."},
 
-    {name:"Fattoush Salad",price:"",
-    img:"menu/appetizer/Fattoush Salad.webp",
+    {name:"Molokhea and fish",price:"€85.00",
+    img:"/Images/fish with molokhea1.jpg",
     desc:"Purslane, romaine lettuce, cucumber, tomatoes, radish, mint, pita chips, sumac, pomegranate, lemon and extra virgin olive oil. Vegan."}
     ,
-    {name:"Hummus",price:"",
-    img:"menu/appetizer/Hummus.webp",
+    {name:"Cheese Pizza",price:"€80.00",
+    img:"/Images/Cheese-Pizza.jpg",
     desc:"Chickpea puree, tahini, lemon and extra virgin olive oil. Gluten free and vegan"},
 
 
-    {name:"Muhammara",price:"",
-    img:"menu/appetizer/Muhammara.webp",
+    {name:"Pepperoni Pizza",price:"€100.00",
+    img:"/images/pepperoni-pizza.jpg",
     desc:"Walnuts, roasted peppers, pomegranate and extra virgin olive oil. Vegan."},
 
-    {name:"Arnabeet Mekle",price:"",
-    img:"menu/main/Arnabeet Mekle.webp",
+    {name:"Margherita Pizza",price:"€120.00",
+    img:"/Images/MARGHERITA-pizza.jpg",
     desc:"Cauliflower, Aleppo pepper, lemon, garlic yogurt and tahini. Gluten free. Vegetarian."},
 
-    {name:"Batata Harra",price:"",
-    img:"menu/main/Batata Harra.webp",
+    {name:"Veggie Pizza",price:"€200.00",
+    img:"/images/veggie-pizza1.jpg",
     desc:"Aleppo pepper, cilantro, herbs and garlic toum. Gluten free. Vegetarian."},
 
-    {name:"Beef Lule Kebab",price:"",
-    img:"menu/main/Beef Lule Kebab.webp",
+    {name:"Chicken Pizza",price:"€140.00",
+    img:"/Images/CHICKEN-PIZZA1.jpg",
     desc:"Ground Angus beef, parsley and Aleppo pepper. Gluten free."},
 
-    {name:"Lamb Lule Kebab",price:"",
-    img:"menu/main/Lamb Lule Kebab.webp",
+    {name:"New-York Pizza",price:"€75.00",
+    img:"/Images/New-York-Style-Pizza.jpg",
     desc:"Ground lamb, red onion, thyme and sumac. Served Armenian pilaf, toasted vermicelli, almonds and grilled veggies."},
 
-    {name:"Mante",price:"",
-    img:"menu/main/Mante.webp",
+    {name:"Smoked Salmon Pizza",price:"€240.00",
+    img:"/Images/smoked-salmon-pizza.jpg",
     desc:"Armenian beef dumpling, garlic yogurt sauce and sumac."},
-
-    {name:"Shawarma",price:"",
-    img:"menu/main/Shawarma.webp",
-    desc:"Tomato, onion, tahini, sumac, pickles. Gluten free."},
-
-    {name:"Shish Taouk",price:"",
-    img:"menu/main/Shish Taouk.webp",
-    desc:"Organic chicken breast and garlic toum. Gluten free."},
-
-    {name:"Vegetarian Flatbread",price:"",
-    img:"menu/main/Vegetarian Flatbread.webp",
-    desc:"Kashkaval cheese, Kalamata olives, cherry tomato, mushrooms, jalapenos and avocado. Vegetarian."},
-
-    {name:"Sujuk Flatbread",price:"",
-    img:"menu/main/Sujuk Flatbread.webp",
-    desc:"Spicy Armenian sausage, akawi cheese, halloumi cheese, 63-degree quail eggs, cherry tomatoes and basil"},
-
-    {name:"Za'atar Flatbread",price:"",
-    img:"menu/main/Za'atar Flatbread.webp",
-    desc:"Aleppo, za'atar, akawi cheese, cherry tomato, mint and extra virgin olive oil. Vegetarian."},
 ];
